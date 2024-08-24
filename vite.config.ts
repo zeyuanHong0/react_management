9b4bdd5d05fb423d0f7113f7f07f3f15e0bbcbc3
Alexander Smith <hongzeyuan0@gmail.com>
2024-08-24 10:45:38 +0800

    vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()); // 获取环境变量
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"), // 相对路径别名配置，使用 @ 代替 src
+      },
+    },
+    server: {
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          target: env.VITE_SERVE,
+          changeOrigin: true,
+          rewrite: (path) =>
+            path.replace(new RegExp(`^${env.VITE_APP_BASE_API}`), ""),
+        },
+      },
+    },
+  };
+});
