76c6d59f3c39d3d8060ad75392433c789eb21d0c
Alexander Smith <hongzeyuan0@gmail.com>
2024-09-06 20:27:58 +0800

    tabbar 全屏按钮

--- src/pages/layout/tabbar/index.tsx
+++ src/pages/layout/tabbar/index.tsx
@@ -1,14 +1,30 @@
 import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
 import { useState } from "react";
+import { FullscreenOutlined, FullscreenExitOutlined } from "@ant-design/icons";
 
 import useStore from "@/store";
 
 const Tabbar = () => {
   const { isFold, setFold } = useStore();
+  const [fullScreen, setFullScreen] = useState<boolean>(false);
+
+  const handleFullScreen = () => {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else {
+      document.documentElement.requestFullscreen();
+    }
+    setFullScreen(!fullScreen);
+  };
+  const Icon = fullScreen ? FullscreenExitOutlined : FullscreenOutlined;
+
   return (
     <div className="w-full h-full flex justify-between items-center bg-[rgb(247, 245, 245)] pl-[10px] pr-[10px]">
       <div className="cursor-pointer" onClick={() => setFold(!isFold)}>
         {isFold ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+      </div>
+      <div>
+        <Icon onClick={handleFullScreen} className="cursor-pointer" />
       </div>
     </div>
   );
