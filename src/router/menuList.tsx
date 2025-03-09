c560d450f0421e17f612d1a212ad7e14e31d5ddc
Alexander Smith <hongzeyuan0@gmail.com>
2025-03-09 19:22:04 +0800

    更新了路由，MenuList

--- src/router/menuList.tsx
+++ src/router/menuList.tsx
@@ -1,15 +1,6 @@
 import {
   HomeOutlined,
   FundProjectionScreenOutlined,
-  LockOutlined,
-  UserOutlined,
-  UserSwitchOutlined,
-  WindowsOutlined,
-  ProductOutlined,
-  TrademarkCircleOutlined,
-  BookOutlined,
-  LaptopOutlined,
-  RestOutlined,
   LineChartOutlined,
 } from "@ant-design/icons";
 export const menuRoutes = [
@@ -37,54 +28,5 @@ export const menuRoutes = [
     label: "数据大屏",
     key: "/screen",
     icon: <FundProjectionScreenOutlined />,
-  },
-  {
-    label: "权限管理",
-    key: "/acl",
-    icon: <LockOutlined />,
-    children: [
-      {
-        label: "用户管理",
-        key: "/acl/user",
-        icon: <UserOutlined />,
-      },
-      {
-        label: "角色管理",
-        key: "/acl/role",
-        icon: <UserSwitchOutlined />,
-      },
-      {
-        label: "菜单管理",
-        key: "/acl/permission",
-        icon: <WindowsOutlined />,
-      },
-    ],
-  },
-  {
-    label: "商品管理",
-    key: "/product",
-    icon: <ProductOutlined />,
-    children: [
-      {
-        label: "品牌管理",
-        key: "/product/trademark",
-        icon: <TrademarkCircleOutlined />,
-      },
-      {
-        label: "属性管理",
-        key: "/product/attr",
-        icon: <BookOutlined />,
-      },
-      {
-        label: "SPU管理",
-        key: "/product/spu",
-        icon: <LaptopOutlined />,
-      },
-      {
-        label: "SKU管理",
-        key: "/product/sku",
-        icon: <RestOutlined />,
-      },
-    ],
   },
 ];
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -8,13 +8,6 @@ const Workbench = lazy(() => import("@/pages/dashboard/workbench"));
 const Analysis = lazy(() => import("@/pages/dashboard/analysis"));
 const Home = lazy(() => import("@/pages/home"));
 const Screen = lazy(() => import("@/pages/screen"));
-const User = lazy(() => import("@/pages/acl/user"));
-const Role = lazy(() => import("@/pages/acl/role"));
-const Permission = lazy(() => import("@/pages/acl/permission"));
-const Trademark = lazy(() => import("@/pages/product/trademark"));
-const Attr = lazy(() => import("@/pages/product/attr"));
-const Spu = lazy(() => import("@/pages/product/spu"));
-const Sku = lazy(() => import("@/pages/product/sku"));
 
 const routes = [
   {
@@ -66,65 +59,6 @@ const routes = [
         element: (
           <Suspense>
             <Screen />
-          </Suspense>
-        ),
-      },
-      // 用户管理
-      {
-        path: "/acl/user",
-        element: (
-          <Suspense>
-            <User />
-          </Suspense>
-        ),
-      },
-      // 权限管理
-      {
-        path: "/acl/role",
-        element: (
-          <Suspense>
-            <Role />
-          </Suspense>
-        ),
-      },
-      // 菜单管理
-      {
-        path: "/acl/permission",
-        element: (
-          <Suspense>
-            <Permission />
-          </Suspense>
-        ),
-      },
-      {
-        path: "/product/trademark",
-        element: (
-          <Suspense>
-            <Trademark />
-          </Suspense>
-        ),
-      },
-      {
-        path: "/product/attr",
-        element: (
-          <Suspense>
-            <Attr />
-          </Suspense>
-        ),
-      },
-      {
-        path: "/product/spu",
-        element: (
-          <Suspense>
-            <Spu />
-          </Suspense>
-        ),
-      },
-      {
-        path: "/product/sku",
-        element: (
-          <Suspense>
-            <Sku />
           </Suspense>
         ),
       },
