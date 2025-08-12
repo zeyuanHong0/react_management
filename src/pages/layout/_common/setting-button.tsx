df8523095ed6466e5f7c5701eedf8c1936b48ad5
Alexander Smith <hongzeyuan0@gmail.com>
2025-08-12 21:17:49 +0800

    点击 header 的设置按钮出现右侧抽屉

--- src/pages/layout/_common/setting-button.tsx
+++ src/pages/layout/_common/setting-button.tsx
@@ -9,33 +9,80 @@ import Color from "color";
 import { CSSProperties, useState } from "react";
 import { motion } from "framer-motion";
 
-import { IconButton, SvgIcon } from "@/components/icon";
+import CyanBlur from "@/assets/images/cyan-blur.png";
+import RedBlur from "@/assets/images/red-blur.png";
+import { IconButton, SvgIcon } from "@/components/Icon";
 import { useThemeToken } from "@/theme/hooks";
 
 const SettingButton = () => {
+  const [openDrawer, setDrawerOpen] = useState(false);
+  const {
+    colorPrimary,
+    colorBgBase,
+    colorTextSecondary,
+    colorTextTertiary,
+    colorBgContainer,
+  } = useThemeToken();
+  const style: CSSProperties = {
+    backdropFilter: "blur(20px)",
+    backgroundImage: `url("${CyanBlur}"), url("${RedBlur}")`,
+    backgroundRepeat: "no-repeat, no-repeat",
+    backgroundColor: Color(colorBgContainer).alpha(0.9).toString(),
+    backgroundPosition: "right top, left bottom",
+    backgroundSize: "50, 50%",
+  };
   return (
-    <div className="flex items-center justify-center">
-      <motion.div
-        animate={{
-          rotate: [0, 360],
+    <>
+      <div className="flex items-center justify-center">
+        <motion.div
+          animate={{
+            rotate: [0, 360],
+          }}
+          transition={{
+            duration: 12,
+            ease: "linear",
+            repeat: Infinity,
+          }}
+          whileTap="tap"
+          variants={{
+            tap: { scale: 0.9 },
+            hover: { scale: 1.1 },
+          }}
+          whileHover="hover"
+          onClick={() => setDrawerOpen(true)}
+        >
+          <IconButton className="h-10 w-10">
+            <SvgIcon icon="ic-setting" size="24" />
+          </IconButton>
+        </motion.div>
+      </div>
+      <Drawer
+        placement="right"
+        title="Settings"
+        onClose={() => setDrawerOpen(false)}
+        open={openDrawer}
+        closable={false}
+        styles={{
+          body: { padding: 0 },
+          mask: { backgroundColor: "transparent" },
+          header: {
+            backgroundColor: colorBgContainer,
+          },
         }}
-        transition={{
-          duration: 12,
-          ease: "linear",
-          repeat: Infinity,
-        }}
-        whileTap="tap"
-        variants={{
-          tap: { scale: 0.9 },
-          hover: { scale: 1.1 },
-        }}
-        whileHover="hover"
+        style={style}
+        width={280}
+        extra={
+          <IconButton
+            onClick={() => setDrawerOpen(false)}
+            className="h-9 w-9 hover:scale-105"
+          >
+            <CloseOutlined className="text-gray-400" />
+          </IconButton>
+        }
       >
-        <IconButton className="h-10 w-10">
-          <SvgIcon icon="ic-setting" size="24" />
-        </IconButton>
-      </motion.div>
-    </div>
+        <div></div>
+      </Drawer>
+    </>
   );
 };
 
